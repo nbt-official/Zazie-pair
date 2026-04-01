39d4ef60bb9c83dc224d2ef573e361ac75bd982e
nbt-official <Anujiimalsha01@gmail.com>
2026-04-01 08:53:35 +0530

    Update and rename mega.js to db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+// db.js
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema({
+    phoneNumber: { type: String, required: true, unique: true },
+    sessionId: { type: String, required: true, unique: true },
+    creds: { type: String, required: true } // JSON eka string ekak widiyata thiyagamu
+});
+
+export const Session = mongoose.model("Session", sessionSchema);
+
+export const connectDB = async (uri) => {
+    try {
+        await mongoose.connect(uri);
+        console.log("✅ MongoDB Connected Successfully!");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Error:", error);
+    }
+};
